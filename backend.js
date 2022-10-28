127ba91c41878f9886dd8e4e94f8fa52fd473c5b
Zeus Amorim <amorimzeus@gmail.com>
2022-10-28 03:02:17 -0300

    Backend Done
    Now the client is able to fetch properly from the backend

--- backend.js
+++ backend.js
@@ -6,29 +6,20 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 
+app.get('/weather', express.json(), (req, res) => {
 
-app.get('/teste', (req, res) => {
-    res.send('<h1> Server funcionando normalmente </h1>')
-})
-
-app.get('/weather', (req, res) => {
-
-    //let lat = req.body.lat;
-    //let lng = req.body.lng;
+    let lat = req.body.latitude;
+    let lng = req.body.latitude;
     let url = 'https://api.open-meteo.com/v1/forecast';
 
-
-
     axios.get(url, {
-
         params: {
-            latitude: '-14.87',
-            longitude: '-40.81',
-            daily: ['weathercode','temperature_2m_max','temperature_2m_min','windspeed_10m_max'],
+            latitude: lat,
+            longitude: lng,
+            daily: ['weathercode', 'temperature_2m_max', 'temperature_2m_min', 'windspeed_10m_max'],
             timezone: 'auto',
-            current_weather:'true',
-            temperature_unit:'celsius'
-
+            current_weather: 'true',
+            temperature_unit: 'celsius'
         }
     })
         .then((response) => {
@@ -38,32 +29,22 @@ app.get('/weather', (req, res) => {
         })
         .catch(error => console.log(error))
 
-
-
-
-
 })
 
-app.get('/location', (req, res) => {
 
-    const lat = req.query.lat;
-    const lng = req.query.lng;
+app.get('/location', express.json(), (req, res) => {
 
-
+    let lat = req.body.latitude;
+    let lng = req.body.longitude;
     const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
-    const URL = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&result_type=administrative_area_level_2|administrative_area_level_1&key=${API_KEY}`;
+    let URL = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&result_type=administrative_area_level_2|administrative_area_level_1&key=${API_KEY}`
 
     axios(URL)
         .then(response => {
-            res.json(response.data)
-
+            res.send(response.data)
         })
         .catch(error => console.log(error))
 
-
-
 })
-
-
 
 app.listen(PORT, () => console.log(`Server running on port  ${PORT}`));
