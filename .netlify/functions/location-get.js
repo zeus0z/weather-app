f77b6e59b186fef7e2fe1d37c078431c6aae21b6
Zeus Amorim <amorimzeus@gmail.com>
2022-11-05 23:22:45 -0300

    Converted backend into netlify functions
    Also change the script tu use netlify-lambda

---
+++ .netlify/functions/location-get.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+require('dotenv').config();
+
+exports.handler = async (event, context) => {
+
+    const params = JSON.parse(event.body);
+
+    const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+    const URL = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${params.latitude},${params.longitude}&result_type=administrative_area_level_2|administrative_area_level_1&key=${API_KEY}`;
+
+
+
+    try {
+        const response = await axios.get(URL);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(response.data)
+        }
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify(error)
+
+        }
+    }
+}
+
