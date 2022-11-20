b8c0b0dd5ced595bbfb1f022f215228a4156f7ab
Zeus Amorim <amorimzeus@gmail.com>
2022-11-20 17:32:28 -0300

    Created ClickedDay Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import WeatherCodeIcons from './components/WeatherCodeIcons';
+import ClickedDay from './pages/ClickedDay';
 import Weather from './pages/Weather';
 
 function App() {
   return (
     <div className="App">
-      <Weather/>
+      <ClickedDay/>
     </div>
   );
 }
---
+++ src/pages/ClickedDay.js
@@ -0,0 +1,9 @@
+const ClickedDay = () => {
+  return (
+    <section>
+        <h1> Teste </h1>
+    </section>
+  )
+}
+
+export default ClickedDay
